ae5f473845a1efb0a8b3d368d6e58ba83a9af988
Gabriel Garcia conde <gabriel@gconde.com>
2017-03-07 18:01:13 -0500

    avance 7 marzo 2017
    mas en responsivo

---
+++ murach/html/chapter-08/responsive.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>San Joaquin Valley Town Hall</title>
+	<meta charset="utf-8"><link rel="shortcut icon" href="img/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/responsive.css">
+</head>
+	
+<body>
+	<header class="jumbotron">
+		<h2>Header</h2>
+	</header>
+	<main>
+		<h2>Main content</h2>
+		<p>lorem ipsum</p>
+		
+		<?php include('inc/carrusel.php'); ?>
+	
+	</main>	
+	<aside>
+		<h2>aside</h2>
+	</aside>
+	<footer>
+		<h2>footer</h2>
+	</footer>
+	
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
